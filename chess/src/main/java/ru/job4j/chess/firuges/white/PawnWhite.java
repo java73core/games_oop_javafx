cc092e2d54879a5773f7c459330fb47a2bd75b75
Andrew Mironov <mrvandrey@gmail.com>
2022-01-18 18:06:33 +0400

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/firuges/white/PawnWhite.java
+++ chess/src/main/java/ru/job4j/chess/firuges/white/PawnWhite.java
@@ -4,8 +4,6 @@ import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
 /**
- * //TODO add comments.
- *
  * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
--- seabattle/src/main/java/ru/job4j/tictactoe/ships/Battleship.java
+++ seabattle/src/main/java/ru/job4j/tictactoe/ships/Battleship.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * //TODO add comments.
- *
  * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
